0f51d0b4f84dbbf129bed4d87726b08687316dcf
Sakshi Priya <priyasakshi8989@gmail.com>
2024-05-16 23:29:44 +0530

    Dice rolling using class component done

---
+++ src/Components/Die/Die.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Die.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class Die extends Component {
+    render(){
+        const {face, rolling} = this.props
+
+        //Using font awesome Icon to show the exact number of dots
+        return(
+            <div>
+                <FontAwesomeIcon icon={['fas', `fa-dice-${face}`]} className={`Die ${rolling && 'Die-shaking'}`}/>
+            </div>
+        )
+    }
+}
+
+export default Die;
